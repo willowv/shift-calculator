e3c077e6ed18b452510103f244f5e9f546872fa9
Willow Vaughan (they/them) <eps.willow@gmail.com>
2021-06-18 16:26:20 -0700

    Parse entities and generate shifts

--- calculateShifts.py
+++ calculateShifts.py
@@ -1,6 +1,6 @@
 import sys
 import csv
-from datetime import date
+from datetime import date, timedelta
 
 class Entity:
     def __init__(self, name, group, days, daysUnavailable):
@@ -18,6 +18,8 @@ class Entity:
     def assigned(self, date, day):
         self.lastShift = date
         self.countPerDay[day] += 1
+    def toString(self):
+        return "{0},{1},{2}".format(self.group, self.name, self.daysUnavailable)
 
 class Shift:
     def __init__(self, date, day):
@@ -34,17 +36,29 @@ class Shift:
             return "{0},{1},{2},{3}".format(self.date, self.day, self.assigned.group, self.assigned.name)
 
 if len(sys.argv) != 6:
-    print("The following arguments are required: \nBoats filename, Start date (YYYY-MM-DD), Number of weekends, Patrol days, Minimum number of weeks between patrols")
+    print("The following arguments are required: \Entities filename, Start date (YYYY-MM-DD), Number of weeks, Days, Minimum number of days between shifts")
     exit()
 
 #sys.argv[0] is the .py file
-boatsFile = sys.argv[1]
+entitiesFile = sys.argv[1]
 dateStart = date.fromisoformat(sys.argv[2])
-numWeekends = sys.argv[3]
-arrPatrolDays = sys.argv[4].split(',')
-numWeeksBetweenShifts = sys.argv[5]
+numWeeks = int(sys.argv[3])
+days = sys.argv[4].split(',')
+numDaysBetweenShifts = int(sys.argv[5])
 
-with open(boatsFile) as bf:
+arrEntities = []
+with open(entitiesFile) as bf:
     bfr = csv.DictReader(bf)
     for row in bfr:
-        print(row['Name'], row['Group'], row['Days Unavailable'])
+        entity = Entity(row['Name'], row['Group'], days, row['Days Unavailable'].split(';'))
+        arrEntities.append(entity)
+        print(entity.toString())
+
+arrShifts = []
+numDays = numWeeks * 7
+for i in range(numDays):
+    shiftDate = dateStart + timedelta(i)
+    shiftDay = shiftDate.strftime('%a').lower()
+    if shiftDay in days:
+        shift = Shift(shiftDate, shiftDay)
+        print(shift.toString())
